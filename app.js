a2fe2d52a92522b5505953c46f66acd6141f4f7e
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-11-04 23:53:34 +0500

    day 1 operators

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // Problem No: 01
 // Days 0: HELLO WORLD
 
-// console.log('Hello, World!');
-// console.log(parameterVariable);
+console.log('Hello, World!');
+console.log(parameterVariable);
 
 
 // Problem No: 02
 // Days 0: DATA TYPES
 
 
-// console.log(firstInteger+Number(secondInteger));
-// console.log(firstDecimal+Number(secondDecimal));
-// console.log(firstDecimal+Number(secondDecimal));
+console.log(firstInteger+Number(secondInteger));
+console.log(firstDecimal+Number(secondDecimal));
+console.log(firstDecimal+Number(secondDecimal));
+
+
+// Problem No: 03
+// Days 1: ARITHMETIC OPERTORS
+
+area = length * width
+perimeter = 2*(length+width)
+
+
