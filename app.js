cadf85a298feb9e9e74fef8cc88bb005de8525d0
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-11-07 09:31:48 +0500

    added day4 13 problm count obj

--- app.js
+++ app.js
@@ -176,3 +176,13 @@ function Rectangle(a, b) {
   }
   return obj;
 }
+
+// Problem No: 13
+// Day 4: Count Objects
+
+function getCount(objects) {
+  let res = 0;
+  for(let i=0; i< objects.length; i++){
+      if(objects[i].x === objects[i].y) res++
+  }return res;
+}
