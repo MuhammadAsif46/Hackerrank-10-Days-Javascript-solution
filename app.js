4882b8d991f5b79ce512724761b7acbc52c63842
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-11-10 11:28:09 +0500

    added day: 9 problm: 25 binary calculator html and css

--- app.js
+++ app.js
@@ -362,35 +362,38 @@
 // Problem No: 24
 // Day 8: Buttons Container
 
-const rotate = () => {
-  for(let i=1; i<=9; i++){
-    if(document.getElementById("btn"+i).innerText == 1){
-      document.getElementById("btn"+i).innerText = 4;
-    }
-    else if(document.getElementById("btn"+i).innerText == 4){
-      document.getElementById("btn"+i).innerText = 7;
-    }
-    else if(document.getElementById("btn"+i).innerText == 7){
-      document.getElementById("btn"+i).innerText = 8;
-    }
-    else if(document.getElementById("btn"+i).innerText == 8){
-      document.getElementById("btn"+i).innerText = 9;
-    }
-    else if(document.getElementById("btn"+i).innerText == 9){
-      document.getElementById("btn"+i).innerText = 6;
-    }
-    else if(document.getElementById("btn"+i).innerText == 6){
-      document.getElementById("btn"+i).innerText = 3;
-    }
-    else if(document.getElementById("btn"+i).innerText == 3){
-      document.getElementById("btn"+i).innerText = 2;
-    }
-    else if(document.getElementById("btn"+i).innerText == 2){
-      document.getElementById("btn"+i).innerText = 1;
-    }
-  }
-}
+// const rotate = () => {
+//   for(let i=1; i<=9; i++){
+//     if(document.getElementById("btn"+i).innerText == 1){
+//       document.getElementById("btn"+i).innerText = 4;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 4){
+//       document.getElementById("btn"+i).innerText = 7;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 7){
+//       document.getElementById("btn"+i).innerText = 8;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 8){
+//       document.getElementById("btn"+i).innerText = 9;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 9){
+//       document.getElementById("btn"+i).innerText = 6;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 6){
+//       document.getElementById("btn"+i).innerText = 3;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 3){
+//       document.getElementById("btn"+i).innerText = 2;
+//     }
+//     else if(document.getElementById("btn"+i).innerText == 2){
+//       document.getElementById("btn"+i).innerText = 1;
+//     }
+//   }
+// }
 
+
+// Problem No: 25
+// Day 8: Binary Calculator
 
 
 
