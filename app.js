abe6b64206d95c54e0b760e0dd4a45015b8afaca
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-11-10 10:30:55 +0500

    added day 8 : problm 24 buttons container

--- app.js
+++ app.js
@@ -359,6 +359,38 @@
 // }
 
 
+// Problem No: 24
+// Day 8: Buttons Container
+
+const rotate = () => {
+  for(let i=1; i<=9; i++){
+    if(document.getElementById("btn"+i).innerText == 1){
+      document.getElementById("btn"+i).innerText = 4;
+    }
+    else if(document.getElementById("btn"+i).innerText == 4){
+      document.getElementById("btn"+i).innerText = 7;
+    }
+    else if(document.getElementById("btn"+i).innerText == 7){
+      document.getElementById("btn"+i).innerText = 8;
+    }
+    else if(document.getElementById("btn"+i).innerText == 8){
+      document.getElementById("btn"+i).innerText = 9;
+    }
+    else if(document.getElementById("btn"+i).innerText == 9){
+      document.getElementById("btn"+i).innerText = 6;
+    }
+    else if(document.getElementById("btn"+i).innerText == 6){
+      document.getElementById("btn"+i).innerText = 3;
+    }
+    else if(document.getElementById("btn"+i).innerText == 3){
+      document.getElementById("btn"+i).innerText = 2;
+    }
+    else if(document.getElementById("btn"+i).innerText == 2){
+      document.getElementById("btn"+i).innerText = 1;
+    }
+  }
+}
+
 
 
 
